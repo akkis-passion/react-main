bdb226cd92f0f994325d4d0044ac325b3d87ea2c
akkis passion <akkib173@icloud.com>
2024-02-21 21:29:12 +0530

    update

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          this is my firs page
+          this is my first page
         </a>
       </header>
     </div>
