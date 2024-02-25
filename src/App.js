34c765e5f542f3261f74265b1f8bd7fafac618be
akkis passion <akkib173@icloud.com>
2024-02-25 23:34:34 +0530

    navbar stntax

--- src/App.js
+++ src/App.js
@@ -3,25 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <>
-    <h1>hii this is akshay</h1>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          this is my first page
-        </a>
-      </header>
+    <div className="header">
+      <div className="nav-bar">
+        <ul className="list-item">
+          <li>Home</li>
+          <li>About</li>
+          <li>Product</li>
+          <li>Contact</li>
+
+        </ul>
+      </div>
     </div>
-    </>
+
   );
 }
 
